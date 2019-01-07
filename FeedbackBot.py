49a19e8a5c98caad08fb3bc0d41afcdf40fbb63a
Oskar GierszewskiOska <gierszon10@gmail.com>
2019-01-07 17:55:27 +0100

    First Commit

---
+++ FeedbackBot.py
@@ -0,0 +1,92 @@
+from selenium import webdriver
+from selenium.webdriver.remote.webdriver import WebDriver
+import json
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+import selenium.webdriver.support.ui as ui
+from selenium.webdriver.common.keys import Keys
+from faker import Faker
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.chrome.options import Options
+import faker.providers as providers
+from random import randint
+import random
+from tkinter import Tk 
+import re
+from decimal import Decimal
+import email
+import imaplib
+from bs4 import BeautifulSoup as BS
+import os
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import requests
+import pyperclip
+from random import choice
+from datetime import datetime
+from re import sub
+
+def send(driver, cmd, params={}):
+  resource = "/session/%s/chromium/send_command_and_get_result" % driver.session_id
+  url = driver.command_executor._url + resource
+  body = json.dumps({'cmd': cmd, 'params': params})
+  response = driver.command_executor._request('POST', url, body)
+  if response['status']:
+    raise Exception(response.get('value'))
+  return response.get('value')
+
+def add_script(driver, script):
+  send(driver, "Page.addScriptToEvaluateOnNewDocument", {"source": script})
+
+def getIP():
+    r = requests.get('https://ipinfo.io/ip',timeout=3)
+    return r.text
+
+def next_available_row(worksheet):
+    str_list = list(filter(None, sheet.col_values(1)))  # fastest
+    return str(len(str_list)+1)
+
+scriptjs=""
+with open('script.js', 'r') as myfile:
+  scriptjs= myfile.read()
+WebDriver.add_script = add_script
+
+with open(("accounts.txt")) as f: #first line-password, next lines - emails
+    line = f.readlines()
+emails = [x.strip() for x in line] 
+password=emails[0]
+email=emails[1] #currently one email supported
+
+print("email: "+str(email))
+print("password: "+str(password))
+IP=getIP()
+print("IP: "+ str(IP[:-1]))
+
+
+timeout = 10
+
+# launch Chrome
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('log-level=3')
+
+driver = webdriver.Chrome(options=chrome_options)
+
+
+# add a script which will be executed when the page starts loading -> makes selenium harder to detect
+driver.add_script(scriptjs)
+
+driver.get("https://login.aliexpress.com/buyer.htm")
+
+time.sleep(1)
+action  = ActionChains(driver)
+action = action.send_keys(Keys.TAB *3)
+action = action.send_keys(email)
+action = action.send_keys(Keys.TAB)
+action = action.send_keys(password)
+action = action.send_keys(Keys.ENTER)
+action.perform()
